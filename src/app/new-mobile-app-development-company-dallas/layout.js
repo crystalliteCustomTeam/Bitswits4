d591ba86d4dcfc96936ae877c4c79f5c8575b9cc
dev-sikander <sikander@crystallitedigital.com>
2024-04-16 06:18:00 +0500

    new pages added

--- src/app/new-mobile-app-development-company-dallas/layout.js
+++ src/app/new-mobile-app-development-company-dallas/layout.js
@@ -1,19 +1,19 @@
 //===== Meta Data =====
 export const metadata = {
   //===== Meta Tags =====
-  title: "Mobile App Development Company Dubai | BitsWits",
+  title: "Mobile App Development Company Dallas | BitsWits",
   description: "Looking for a top-rated web app development company? Our team of skilled web app developers specializes in crafting high-quality and customized web applications.",
   //===== OG Tags =====
   openGraph: {
-    title: 'Mobile App Development Company Dubai | BitsWits',
+    title: 'Mobile App Development Company Dallas | BitsWits',
     description: 'Looking for a top-rated web app development company? Our team of skilled web app developers specializes in crafting high-quality and customized web applications.',
-    url: '/new-mobile-app-development-company-dubai',
+    url: '/new-mobile-app-development-company-dallas',
     siteName: 'BitsWits',
     locale: 'en_US',
     type: 'website',
   },
   //===== Canonical =====
-  alternates: { canonical: '/new-mobile-app-development-company-dubai' },
+  alternates: { canonical: '/new-mobile-app-development-company-dallas' },
   //===== GEO Tags =====
   other: {},
 }
