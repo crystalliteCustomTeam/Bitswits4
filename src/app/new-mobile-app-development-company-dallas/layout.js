0ceaf291b6ff5a70ec687210ddb62444be892bdd
dev-sikander <sikander@crystallitedigital.com>
2024-04-17 05:21:14 +0500

    dallas page add

---
+++ src/app/new-mobile-app-development-company-dallas/layout.js
@@ -0,0 +1,23 @@
+//===== Meta Data =====
+export const metadata = {
+  //===== Meta Tags =====
+  title: "Mobile App Development Company Newyork | BitsWits",
+  description: "Looking for a top-rated web app development company? Our team of skilled web app developers specializes in crafting high-quality and customized web applications.",
+  //===== OG Tags =====
+  openGraph: {
+    title: 'Mobile App Development Company Newyork | BitsWits',
+    description: 'Looking for a top-rated web app development company? Our team of skilled web app developers specializes in crafting high-quality and customized web applications.',
+    url: '/new-mobile-app-development-company-newyork',
+    siteName: 'BitsWits',
+    locale: 'en_US',
+    type: 'website',
+  },
+  //===== Canonical =====
+  alternates: { canonical: '/new-mobile-app-development-company-newyork' },
+  //===== GEO Tags =====
+  other: {},
+}
+
+export default function RootLayout({ children }) {
+  return (children);
+}
