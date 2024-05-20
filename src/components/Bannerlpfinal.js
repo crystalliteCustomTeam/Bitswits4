2c2c272976aa4dbe03fe13346d67d84d66797403
cl-farooqaziz <dev.faz@productions-dev.com>
2024-05-21 03:33:08 +0500

    new changes

--- src/components/Bannerlpfinal.js
+++ src/components/Bannerlpfinal.js
@@ -105,10 +105,8 @@ const Bannerlpfinal = (props) => {
     return (
         <>
 
-            <section className={styles[props.newHomeBgprops]} >
-
+            <section className={`${styles[props.newHomeBgprops]} d-none d-lg-block`} >
                 <Container className={` ${styles.conform1} overflow-hidden`}>
-
                     <Row className={`${styles.alignpost} gx-5`}>
                         <Col xl={7}>
                             <div className={` ${styles.conform} ${styles.oppp}`}>
@@ -121,35 +119,32 @@ const Bannerlpfinal = (props) => {
                                 </div>
                             </div>
                             <div className={styles.comple}>
-                                <Image loading="lazy" quality={95} alt='BitsWits' src={banImg1} className={`${styles.newone} img-fluid w-25`} />
+                                <Image priority quality={95} alt='BitsWits' src={banImg1} className={`${styles.newone} img-fluid w-25`} />
 
                                 <div className={styles.comple1}>
-                                    <Image loading="lazy" quality={95} alt='BitsWits' src={star} className='img-fluid' />
+                                    <Image priority quality={95} alt='BitsWits' src={star} className='img-fluid' />
                                     <p className='mb-0'> <span className='fw700'>4.9 out of 5</span> <span className={styles.rating}>(review rating) <br></br>
                                         Over 1,200+ reviews
                                         Over 1,200+ reviews</span> </p>
                                 </div>
-                                <Image loading="lazy" quality={95} alt='BitsWits' src={clutch} className='img-fluid' />
+                                <Image priority quality={95} alt='BitsWits' src={clutch} className='img-fluid' />
                             </div>
-
                             <div className={styles.posty}>
                                 <div className={styles.comple3}>
-                                    <Image loading="lazy" quality={95} alt='BitsWits' src={star2} className='img-fluid' />
+                                    <Image priority quality={95} alt='BitsWits' src={star2} className='img-fluid' />
                                     <div className={styles.comple5}>
                                         <p className='mb-0'>US TOP 3 BEST RATED</p>
                                         <p className='mb-0 fw700'>App & Software Development Company</p>
                                     </div>
                                 </div>
                                 <div className={styles.comple3}>
-                                    <Image loading="lazy" quality={95} alt='BitsWits' src={star1} className='img-fluid' />
+                                    <Image priority quality={95} alt='BitsWits' src={star1} className='img-fluid' />
                                     <div className={styles.comple5}>
                                         <p className='mb-0'>HIGH PERFORMER</p>
                                         <p className='mb-0 fw700'>Top App Development Company</p>
                                     </div>
                                 </div>
-
                             </div>
-
                         </Col>
                         <Col xl={5}>
                             <form className={styles.your} onSubmit={handleSubmit}>
@@ -171,15 +166,15 @@ const Bannerlpfinal = (props) => {
                                 </div>
                                 <div className={styles.ouroff}>
                                     <span>
-                                        <Image loading="lazy" quality={95} alt='BitsWits' src={usa} className='img-fluid' />
+                                        <Image priority quality={95} alt='BitsWits' src={usa} className='img-fluid' />
                                         <p>USA</p>
                                     </span>
                                     <span>
-                                        <Image loading="lazy" quality={95} alt='BitsWits' src={dubai} className='img-fluid' />
+                                        <Image priority quality={95} alt='BitsWits' src={dubai} className='img-fluid' />
                                         <p>MIDDLE EAST</p>
                                     </span>
                                     <span>
-                                        <Image loading="lazy" quality={95} alt='BitsWits' src={asia} className='img-fluid' />
+                                        <Image priority quality={95} alt='BitsWits' src={asia} className='img-fluid' />
                                         <p>ASIA</p>
                                     </span>
                                 </div>
@@ -189,8 +184,46 @@ const Bannerlpfinal = (props) => {
                         </Col>
                     </Row>
                 </Container>
-
-
+            </section>
+            <section className={`${styles[props.newHomeBgprops]} d-block d-lg-none`}>
+                <Container className={` ${styles.conform1} overflow-hidden`}>
+                    <Row className={`${styles.alignpost} gx-5`}>
+                        <Col xl={7}>
+                            <div className={` ${styles.conform} ${styles.oppp}`}>
+                                <h1 className='font-bold white f-25'>Mobile App Development Services</h1>
+                                <h2 className='white font50 f-700'>We Don't Just <span className={styles.corpost}>Create Apps</span>, We <span className={styles.corpost}>Create Experiences</span> </h2>
+                                <p className='font16 white fw400 mt-4 mb-lg-5'>Transform your idea into a remarkable app with Bitswits Mobile app development services. Our creative and efficient development process caters to both native and cross-platform needs, ensuring your app makes a lasting impact.</p>
+                                <div className={styles.careatapp}>
+                                    <a href='tel:8335006007' className={styles.delivernow}>CALL NOW</a>
+                                    <a href='javascript:$zopim.livechat.window.show();' className={styles.deliver}>LIVE CHAT</a>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <section className={`bg-black d-block d-lg-none`}>
+                <Container className={` ${styles.conform1} overflow-hidden`}>
+                    <Row className={`${styles.alignpost} gx-5`}>
+                        <Col xl={5}>
+                            <form className={styles.your} onSubmit={handleSubmit}>
+                                <h3 className='white fw500'>Transform Your App Idea Into A Success Story</h3>
+                                <h2 className='mb-3 fw500 white'>Today Is The Day To Start!</h2>
+                                <input type='text' minLength="4" name='first' required className={styles.newform} placeholder="Your Name*"></input>
+                                <input type='email' name='email' required className={styles.newform} placeholder="Your Email*"></input>
+                                <input type="tel" minLength="10" maxLength="13" pattern="[0-9]*" name='phone' required className={styles.newform} placeholder="Your Phone Number*"></input>
+                                <textarea placeholder='Message*' required name='message' className={styles.newformtext}></textarea>
+                                <div className={styles.takesread}>
+                                    <p>
+                                        We takes yout privacy seriously. Read our
+                                        Privacy Notice.
+                                    </p>
+                                    <input type='submit' name='submit' className={styles.value} placeholder="Submit" value={score}></input>
+                                </div>
+                            </form>
+                        </Col>
+                    </Row>
+                </Container>
             </section>
         </>
     )
