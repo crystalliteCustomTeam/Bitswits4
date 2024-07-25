8abca8393dc249cc71b0fd17f0f6a665a013f4b6
cl-farooqaziz <dev.faz@productions-dev.com>
2024-07-25 05:39:40 +0500

    casestudy section done

---
+++ src/app/casestudy-section/page.js
@@ -0,0 +1,11 @@
+//Import Components
+import CaseStudySection from "@/src/components/CaseStudySection";
+
+
+export default function Page() {
+  return (
+    <>
+      <CaseStudySection />
+    </>
+  );
+}
